2982e4183d936cbed98f228dffaac600e4e97281
unknown <cami.rivas.dlb@gmail.com>
2025-06-28 10:14:28 -0400

    Primer commit

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const app = express();
 const reservasRoutes = require("./assets/routes/routes.js");
@@ -5,7 +6,7 @@ const reservasRoutes = require("./assets/routes/routes.js");
 app.use(express.json());
 app.use("/api/reservas", reservasRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT ;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
