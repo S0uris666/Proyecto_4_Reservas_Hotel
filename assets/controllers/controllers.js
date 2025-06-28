cc663e46f367ec3929c9680aea7291181d9fd335
unknown <cami.rivas.dlb@gmail.com>
2025-06-28 14:23:40 -0400

    se agregó async a controller

--- assets/controllers/controllers.js
+++ assets/controllers/controllers.js
@@ -1,17 +1,37 @@
+const fs= require('fs');
+const path = require('path');
 let reservas = [];
+
+const filePath = path.join(__dirname, '../data/reservas.json');
+
+const leerReservas = async () => {
+  try {
+    const data = await fs.readFile(filePath, 'utf-8');
+    return JSON.parse(data);
+  } catch (err) {
+    return [];
+  }
+};
+
+const guardarReservas = async (reservas) => {
+  await fs.writeFile(filePath, JSON.stringify(reservas, null, 2));
+};
+
 let idCounter = 1;
 
-const crearReserva = (req, res) => {
-  const nuevaReserva = {
+const crearReserva = async(req, res) => {
+   const reservas = await leerReservas();
+   const nuevaReserva = {
     id: idCounter++,
     ...req.body,
   };
   reservas.push(nuevaReserva);
+  await guardarReservas(reservas);
   res.status(201).json(nuevaReserva);
 };
 
-const obtenerReservas = (req, res) => {
-  let resultado = reservas;
+const obtenerReservas = async(req, res) => {
+  let resultado = await leerReservas();
   const { hotel, fecha_inicio, fecha_fin, tipo_habitacion, estado, num_huespedes } = req.query;
 
   if (hotel) resultado = resultado.filter(r => r.hotel === hotel);
@@ -28,23 +48,28 @@ const obtenerReservas = (req, res) => {
   res.json(resultado);
 };
 
-const obtenerReservaPorId = (req, res) => {
+const obtenerReservaPorId = async(req, res) => {
+  const reservas= await leerReservas();
   const reserva = reservas.find(r => r.id == req.params.id);
   if (!reserva) return res.status(404).json({ error: "Reserva no encontrada" });
   res.json(reserva);
 };
 
-const actualizarReserva = (req, res) => {
+const actualizarReserva = async(req, res) => {
+  const reservas = await leerReservas();
   const index = reservas.findIndex(r => r.id == req.params.id);
   if (index === -1) return res.status(404).json({ error: "Reserva no encontrada" });
   reservas[index] = { ...reservas[index], ...req.body };
+  await guardarReservas(reservas);
   res.json(reservas[index]);
 };
 
-const eliminarReserva = (req, res) => {
+const eliminarReserva = async(req, res) => {
+  const reservas = await leerReservas();
   const index = reservas.findIndex(r => r.id == req.params.id);
   if (index === -1) return res.status(404).json({ error: "Reserva no encontrada" });
   reservas.splice(index, 1);
+  await guardarReservas(reservas);
   res.status(204).send();
 };
 
